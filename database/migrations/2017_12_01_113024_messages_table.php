1f8eb43c74e0a4b2569c29e952e78d02c3e8f58d
Ik0n <Ik0n16111998@gmail.com>
2017-12-01 18:24:35 +0300

    Fix messages

--- database/migrations/2017_12_01_113024_messages_table.php
+++ database/migrations/2017_12_01_113024_messages_table.php
@@ -13,7 +13,7 @@ class MessagesTable extends Migration
      */
     public function up()
     {
-        Schema::create('Messages', function (Blueprint $table) {
+        Schema::create('messages', function (Blueprint $table) {
             $table->increments('id');
 
             $table->integer('user_id', false, true)->default(0);
